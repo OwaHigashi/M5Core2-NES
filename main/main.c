8e5fe4fb0bc312e06016ef037b7b78e614b0794f
OwaHigashi <187878999+OwaHigashi@users.noreply.github.com>
2022-09-19 15:44:29 +0900

    add nesemu.

--- main/main.c
+++ main/main.c
@@ -162,6 +162,7 @@ void app_main(void)
 	
 	m5core2_init();
 
+#if 0
 	lvgl_i2c_locking(i2c_manager_locking());
 
 	lv_init();
@@ -169,9 +170,12 @@ void app_main(void)
 
 	// Needs to be pinned to a core
 	xTaskCreatePinnedToCore(gui_thread, "gui", 4096*2, NULL, 0, NULL, 1);
+#endif
 
 	printf("Running...\n");
 	fflush(stdout);
+
+	nofrendo_main(0, NULL);
 
 	for ( ; ; ) {
 		vTaskDelay(portMAX_DELAY);
@@ -179,3 +183,47 @@ void app_main(void)
 	printf("Restarting now.\n");
 	esp_restart();
 }
+
+
+//#include "freertos/FreeRTOS.h"
+//#include "esp_wifi.h"
+//#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_event_loop.h"
+#include "nvs_flash.h"
+#include "driver/gpio.h"
+#include "nofrendo.h"
+#include "esp_partition.h"
+
+
+
+char *osd_getromdata() {
+	char* romdata;
+	const esp_partition_t* part;
+	spi_flash_mmap_handle_t hrom;
+	esp_err_t err;
+	nvs_flash_init();
+	part=esp_partition_find_first(0x40, 1, NULL);
+	if (part==0) printf("Couldn't find rom part!\n");
+	err=esp_partition_mmap(part, 0, 3*1024*1024, SPI_FLASH_MMAP_DATA, (const void**)&romdata, &hrom);
+	if (err!=ESP_OK) printf("Couldn't map rom part!\n");
+	printf("Initialized. ROM@%p\n", romdata);
+    return (char*)romdata;
+}
+
+
+esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+    return ESP_OK;
+}
+
+# if 0
+int app_main(void)
+{
+	printf("NoFrendo start!\n");
+	nofrendo_main(0, NULL);
+	printf("NoFrendo died? WtF?\n");
+	asm("break.n 1");
+    return 0;
+}
+#endif 
